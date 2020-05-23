fbc79d4a667614d08be40052bd460f5ddd73c236
Michael Obst <michael.obst@telekom.de>
2020-05-23 21:36:31 +0200

    initial commit

---
+++ src/test/java/de/telekom/sea/mystuff/backend/MyStuffBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.telekom.sea.mystuff.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyStuffBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
