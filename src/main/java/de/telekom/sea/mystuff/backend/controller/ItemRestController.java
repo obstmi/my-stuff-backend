74b59a74333061fedfc6d8690e1bbefd6fbbec70
Michael Obst <michael.obst@telekom.de>
2020-05-25 19:58:29 +0200

    PUT method in controller implemented, HTTP Statuscodes added

--- src/main/java/de/telekom/sea/mystuff/backend/controller/ItemRestController.java
+++ src/main/java/de/telekom/sea/mystuff/backend/controller/ItemRestController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import de.telekom.sea.mystuff.backend.entity.Item;
 import de.telekom.sea.mystuff.backend.repository.ItemRepository;
@@ -45,18 +46,41 @@ public class ItemRestController {
 		return repository.save(newItem);
 	}
 	
-//	// TBD
+	@PutMapping("items/{id}")
+	public Item replaceItem(@RequestBody Item newItem, @PathVariable Long id) {
+		
+		return repository.findById(id).map(dbItem -> {
+			dbItem.setName(newItem.getName());
+			dbItem.setAmount(newItem.getAmount());
+			dbItem.setLocation(newItem.getLocation());
+			dbItem.setDescription(newItem.getDescription());
+			dbItem.setLastUsed(newItem.getLastUsed());
+			return repository.save(dbItem);
+		}).orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+	
+//	// Alternative ohne Lambda (und ohne Fehlerhandling):
 //	@PutMapping("items/{id}")
-//	public Item replaceItem(@RequestBody Item newItem, @PathVariable Long id) {
-//		return repository.save(newItem);
+//	public Item editItem(@RequestBody Item newItem, @PathVariable Long id) {
+//
+//		Item item = repository.findById(id).get();
+//		item.setName(newItem.getName());
+//		item.setAmount(newItem.getAmount());
+//		item.setLocation(newItem.getLocation());
+//		item.setDescription(newItem.getDescription());
+//		item.setLastUsed(newItem.getLastUsed());
+//		return repository.save(item);
 //	}
 
-	@DeleteMapping
+	@DeleteMapping("items/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable Long id) {
 		try {
 			repository.deleteById(id);	
 		} catch (EmptyResultDataAccessException e) {
-			throw new ResourceNotFoundException(id);
+			//throw new ResourceNotFoundException(id);
+			// alternativ:
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Konnte Ressource nicht finden: " + id);
 		}
 		
 	}
